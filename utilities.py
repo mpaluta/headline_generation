98b28761c026e4f0ab6d75eba6f9f57c0a4d0dd9
Ben <31463474+arnoldyb@users.noreply.github.com>
2019-07-06 10:10:46 -0700

    win/linux interoperability fix

--- utilities.py
+++ utilities.py
@@ -18,5 +18,6 @@ class data_setup():
         with open(set+"_IDs.csv") as file_path:
             reader = csv.reader(file_path)
             paths = list(reader)
-
+        if [] in paths:
+            paths = paths[::2]
         return paths
