917d1fa8077c4568aa445a0b43ffeb6d36c1354d
mpaluta <markpaluta@gmail.com>
2019-07-17 22:50:39 -0700

    some more to problem

--- Gavrilov.py
+++ Gavrilov.py
@@ -3,7 +3,7 @@
 # prob = text_problems.Text2TextProblem()
 # prob2 = text_problems.Text2textTmpdir(prob)
 
-# import os
+import os
 from tensor2tensor.utils import registry
 from tensor2tensor.data_generators import text_problems #, token_generator, EOS
 from tensor2tensor.data_generators import problem
@@ -15,7 +15,18 @@ class Gavrilov(text_problems.Text2TextProblem):
 
   @property
   def targeted_vocab_size(self):
-    return 40000 #33420
+    return 40000
+
+  @property
+  def is_generate_per_split(self):
+    # generate_data will shard the data into TRAIN and EVAL for us.
+    return False
+
+  def generate_samples(self, data_dir, tmp_dir, dataset_split):
+    input_train = open(os.path.join(tpm_dir,"inputs.train.txt"), "r")
+    input_target = open(os.path.join(tmp_dir,"inputs.target.txt"), "r")
+    yield {"inputs": input_train, "targets": input_target}
+
 
   # @property
   # def vocab_name(self):
