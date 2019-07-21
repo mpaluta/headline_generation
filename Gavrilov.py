901d7dc488906a6ba331f8e77a009a8af9f43336
mpaluta <markpaluta@gmail.com>
2019-07-21 13:57:30 -0700

    Gavrilov subword tokenizer attempt 1

--- Gavrilov.py
+++ Gavrilov.py
@@ -9,13 +9,18 @@ from tensor2tensor.data_generators import text_problems #, token_generator, EOS
 from tensor2tensor.data_generators import problem
 # from tensor2tensor.data_generators import text_encoder
 
+# Define filepaths
+nyt_path = './data/nyt/' # points to folder containing the years folders of the NYT Annotated corpus
+log_path = './logs/' # points to folder containing all the logs
+
 @registry.register_problem
 class Gavrilov(text_problems.Text2TextProblem):
   """Headline generation following along Gavrilov et all."""
 
   @property
   def targeted_vocab_size(self):
-    return 40000
+    return 40000 # 40000 per Gavrilov but powers of 2 are efficient per
+                 # documentation so could also try 32768
 
   @property
   def is_generate_per_split(self):
@@ -25,21 +30,21 @@ class Gavrilov(text_problems.Text2TextProblem):
   def generate_samples(self, data_dir, tmp_dir, dataset_split):
     # might need a little fudging after pipeline set up but should be close
     if dataset_split == problem.DatasetSplit.TRAIN:
-      paths = open(os.path.join(tpm_dir,"meta_train.log"), "r")
+      paths = open(os.path.join('.',"meta_train.log"), "r")
     if dataset_split == problem.DatasetSplit.EVAL:
-      paths = open(os.path.join(tpm_dir,"meta_dev.log"), "r")
+      paths = open(os.path.join('.',"meta_dev.log"), "r")
     # if dataset_split == problem.DatasetSplit.TEST:
     #   train_paths = open(os.path.join(tpm_dir,"meta_test.log"), "r")
-    next_line = paths.readlines()
-    next_line_as_list = next_line.split(",")
-    path = next_line_as_list[0] # assuming first element of log file will be path
-    article = NYTArticle.from_file(os.path.join('data', 'nyt', path) # could replace with tmp_dir or data_dir
-    lede = " ".join(article.lede)
-    # body = " ".join(article.paragraphs) # not using for now
-    headline = article.print_hede[0]
 
-    yield {"inputs": lede, "targets": headline}
-
+  data_df = pd.read_csv(paths, sep=",", header=0,
+                dtype={'filepath': str,'hede_size': int,'wordcount': int,'section': str, 'sent_hede': float, 'sent_lede': float, 'sent_body': float})
+  for index, row in data_df.iterrows():
+     filepath = row['filepath']
+     article = NYTArticle.from_file(os.path.join(nyt_path, filepath))
+     lede = " ".join(article.lede)
+     # body = " ".join(article.paragraphs) # not using for now
+     headline = article.print_hede[0]
+     yield {"inputs": lede, "targets": headline}
 
   # @property
   # def vocab_name(self):
@@ -48,11 +53,12 @@ class Gavrilov(text_problems.Text2TextProblem):
   # @property
   # def input_space_id(self):
   #   return problem.SpaceID.EN_BPE_TOK
+
   #
   # @property
   # def target_space_id(self):
   #   return problem.SpaceID.CS_TOK # TODO CS_BPE_TOK
   #
-  # @property
-  # def use_subword_tokenizer(self):
-  #   return False
+  @property
+  def use_subword_tokenizer(self):
+    return True
